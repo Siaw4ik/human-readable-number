3d7881fcc8ba8865a631784d80b410bc119b29e5
Slawik <peretsviach@gmail.com>
2022-08-06 11:57:55 +0200

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    const one = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ten = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) {
+        return one[number];
+    } else if (number < 100) {
+        return ten[Math.floor(number / 10)] + ((number % 10 === 0) ? '' : ' ' + one[number % 10]);
+    } else {
+        return one[Math.floor(number / 100)] + ' hundred' + ((number % 100 === 0) ? '' : ' ' + toReadable([number % 100]));
+    }
+
 }
